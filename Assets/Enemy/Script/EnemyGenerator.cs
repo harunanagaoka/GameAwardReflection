7832b405c50e8638727a2d1416e1181a4c92a223
litelime <litelime0821@gmail.com>
2025-10-28 15:14:39 +0900

    Enemyに直しました

---
+++ Assets/Enemy/Script/EnemyGenerator.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+//using static BulletBehaviourData;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    //[SerializeField]
+    //private GameObject m_baseBullet;
+
+    //private ConfigData.BulletConfig m_bulletConfig;
+
+    [SerializeField]
+    private Vector3 m_generateOffset = Vector3.zero;
+
+    private void Start()
+    {
+        GameObject config = GameObject.Find("Config");
+        //ConfigManager configManager = config.GetComponent<ConfigManager>();
+
+       // m_bulletConfig = configManager.BulletConfig;
+    }
+
+    //behavior.Typeに応じてコンポーネントを付けた弾を生成、自身のtransformの子にする
+    //public void GenarateEnemy(ConfigData.BulletConfig.Behaviour behaviour,Vector3 spawnPos)
+    //{
+    //    GameObject Enemy = GameObject.Instantiate(m_enemyConfig.Base, transform);
+
+    //    Enemy.transform.position = spawnPos + m_generateOffset;
+    //    //ボスのポジションにする
+
+    //    Vector3 playerPos = PlayerManager.Instance.Players[0].transform.position;
+
+    //    switch (behaviour.Type)
+    //    {
+            
+    //        case EnemyType.Straight:
+    //            StraightMove enemyMove = enemy.AddComponent<StraightMove>();
+    //            enemyMove.Initialize(playerPos, behaviour.Speed);
+    //            EnemyDestroyer destroyerA = enemy.AddComponent<EnemyDestroyer>();
+    //            destroyerA.Initialize(behaviour.Destroy, behaviour.DeadTime);
+    //            break;
+
+    //        case EnemyType.Following:
+    //            FollowingMove followingMove = enemy.AddComponent<FollowingMove>();
+    //            followingMove.Initialize(playerPos, behaviour.Speed);
+    //            EnemyDestroyer destroyerB = enemy.AddComponent<EnemyDestroyer>();
+    //            destroyerB.Initialize(behaviour.Destroy, behaviour.DeadTime);
+
+    //            break;
+    //    }
+    }
+
+    //public void GenarateBullet(BulletBehaviour behavior)
+    //{
+    //    GameObject bullet = GameObject.Instantiate(m_baseBullet, transform);
+
+    //    bullet.transform.position = behavior.SpawnPos;
+
+    //    switch (behavior.Type)
+    //    {
+    //        case BulletBehaviourType.Straight:
+    //            StraightMove bulletMove = bullet.AddComponent<StraightMove>();
+    //            bulletMove.Initialize(behavior.Direction,behavior.Speed);
+
+    //            break;
+
+    //        case BulletBehaviourType.Following:
+    //            FollowingMove followingMove = bullet.AddComponent<FollowingMove>();
+    //            followingMove.Initialize(behavior.Direction, behavior.Speed);
+
+    //            break;
+    //    }
+    //}
+}
---
+++ Assets/Enemy/Script/EnemyMovePattern.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public abstract class EnemyMovePattern : MonoBehaviour
+{
+    protected Rigidbody m_rigidbody;
+
+    protected Vector3 m_direction;
+
+    protected float m_speed;
+    
+    protected bool m_isReflected = false;
+
+  
+    void Awake() 
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void OnReflected(Vector3 normal)
+    {
+        m_isReflected = true;
+        normal = normal.normalized;
+
+        m_direction = Vector3.Reflect(m_direction, normal);
+
+        // ïKóvÇ»ÇÁë¨ìxÇ‡ëùâ¡
+        // m_speed *= 1.2f; // îΩéÀÇÃÇΩÇ—Ç…20%ë¨Ç≠Ç∑ÇÈÇ»Ç«
+
+        //Vector3 velocity = m_rigidbody.linearVelocity;
+        //Vector3 reflectedVelocity = Vector3.Reflect(velocity, normal);
+        //m_rigidbody.linearVelocity = reflectedVelocity * 4.0f;
+    }
+
+    public abstract void Initialize(Vector3 dir,float speed);
+    public abstract void Move();
+
+    public abstract void SetDirection(Vector3 dir);
+
+    public abstract void SetSpeed(float spd);
+}
