20db4388123ad70ca4336192d34d26ab617da2c7
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-11-13 09:28:16 +0900

    アニメーション実装　途中

---
+++ Assets/MyProject/Common/Scripts/AnimationNames.cs
@@ -0,0 +1,9 @@
+
+public static class AnimationNames
+{
+    //Playerのアニメーション
+    public const string Standing = "Standing";
+    public const string Running = "Running";
+    public const string Attack = "Attack";
+    public const string Damage = "Damage";
+}
---
+++ Assets/MyProject/Player/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator m_animator;
+
+    private PlayerEvents m_playerEvents;
+
+    private readonly int m_standingHash = Animator.StringToHash(AnimationNames.Standing);
+    private readonly int m_runningHash = Animator.StringToHash(AnimationNames.Running);
+    private readonly int m_attackHash = Animator.StringToHash(AnimationNames.Attack);
+    private readonly int m_damageHash = Animator.StringToHash(AnimationNames.Damage);
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_playerEvents = GetComponentInParent<PlayerEvents>();
+    }
+
+    void Update()
+    {
+  
+    }
+
+    private void PlayRunning()
+    {
+        m_animator.Play(m_attackHash);
+    }
+
+
+}
