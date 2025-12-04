dee64b41f0e2737a16d686da21013224c504e2f2
litelime <litelime0821@gmail.com>
2025-12-04 15:57:05 +0900

    ロケットパンチ追加

--- Assets/MyProject/Enemy/Script/EnemyAttack/RocketPunchMove.cs
+++ Assets/MyProject/Enemy/Script/EnemyAttack/RocketPunchMove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class RocketPunchMove : MonoBehaviour
 {
@@ -8,9 +9,21 @@ public class RocketPunchMove : MonoBehaviour
     [SerializeField]
     private float m_delayTime;
 
+    [SerializeField]
+    Quaternion rotation;
+
     public float m_speed;
     private Vector3 m_direction;
 
+    private Rigidbody rigid;
+
+
+    
+    private void Start()
+    {
+        Player = PlayerManager.Instance.Players[0];
+        rigid = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
         if(m_delayTime > 0)
@@ -27,10 +40,20 @@ public class RocketPunchMove : MonoBehaviour
     {
         //1.スクリプトがアタッチされているオブジェクトの向きをプレイヤーの方向に向ける
         //2.m_directionをプレイヤーの方向に更新する
+
+        rotation = Quaternion.LookRotation(Player.transform.position - this.transform.position);    // 向きを回転するQuaternion
+        transform.rotation = rotation;
+
     }
 
     public void Move()
     {
         //オブジェクトをm_directionの方向にm_speedの速さで移動させる
+        //rigid.MovePosition(transform.position + transform.forward * m_speed * Time.fixedDeltaTime);
+       transform.position += transform.forward * m_speed * Time.fixedDeltaTime;
+        if(Vector3.Distance(transform.position, Player.transform.position) < 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
