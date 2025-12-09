b3b1bb6369116e034c2aa7ed8280a599803a13e1
karin_miyakawa <miyakawakarinn@gmail.com>
2025-12-09 15:22:12 +0900

    ゲームオーバー＆シーン再度読み込みの完成

---
+++ Assets/MyProject/Scenes/Scripts/ProcessTimeOver.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProcessTimeOver : MonoBehaviour
+{
+    bool isGameOver = false;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        MainGameTimer mgt = GetComponent<MainGameTimer>();
+        if (!isGameOver && mgt.CurrentTime == 0)//CurrentTimeが0以下になったら
+        {
+            Debug.Log("GameOver");
+            isGameOver = true;
+        }
+
+        if (isGameOver&& Input.GetButtonDown("Fire4"))
+        {
+            // 現在のシーン名を取得
+            string currentSceneName = SceneManager.GetActiveScene().name;
+
+            // 同じシーンをロード（再読み込み）
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+}
