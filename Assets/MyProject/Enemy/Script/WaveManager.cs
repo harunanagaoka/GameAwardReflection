c06516ab6c4b34e9d08c13317779154f3ec59db5
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-12-09 14:46:04 +0900

    [Add]敵をカウントする機能の実装

--- Assets/MyProject/Enemy/Script/WaveManager.cs
+++ Assets/MyProject/Enemy/Script/WaveManager.cs
@@ -1,18 +1,35 @@
+using System.Net;
 using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
     EnemyGenerator m_generator;
 
+    private int m_currentEnemyCount = 0;
+
+    private bool m_isCleared = false;
+
+    public bool IsCleared => m_isCleared;
+
     void Start()
     {
         m_generator = GetComponent<EnemyGenerator>();
-        m_generator.GenerateEnemy(Vector3.left);
+        //m_generator.GenerateEnemy(Vector3.left);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        m_isCleared = CheckWaveClear();
+    }
+
+    private bool CheckWaveClear()
+    {
+        if(transform.childCount <= 0)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
