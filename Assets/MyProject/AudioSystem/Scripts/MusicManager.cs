b5e1af0c61b65284be5d419ce6801dfe1688c357
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-12-18 09:19:58 +0900

    [Add]Audio関係のスクリプト調整（作業途中）

--- Assets/MyProject/AudioSystem/Scripts/MusicManager.cs
+++ Assets/MyProject/AudioSystem/Scripts/MusicManager.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
-using static MusicManager;
 
 public class MusicManager : MonoBehaviour
 {
-    [SerializeField]
-    private AudioSource m_audioSources;
+    //MusicはSEとはちがい一度に複数個重なるケースが起こりにくいため、ひとつだけ所持させています。
+    private AudioSource m_audioSource;
 
     [SerializeField]
     private AudioClip[] m_audioClips;
@@ -17,16 +16,22 @@ public class MusicManager : MonoBehaviour
         ResultLoop
     }
 
+    private void Start()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+    }
+
     public void OnPlay(MusicName musicNum)
     {
         OnStop();
-        m_audioSources.clip = m_audioClips[(int)musicNum];
-        m_audioSources.loop = true;
-        m_audioSources.Play();
+        m_audioSource.clip = m_audioClips[(int)musicNum];
+        m_audioSource.loop = true;
+        m_audioSource.Play();
 
     }
+
     public void OnStop()
     {
-        m_audioSources.Stop();
+        m_audioSource.Stop();
     }
 }
--- Assets/MyProject/AudioSystem/Scripts/SEManager.cs
+++ Assets/MyProject/AudioSystem/Scripts/SEManager.cs
@@ -8,6 +8,11 @@ public class SEManager : MonoBehaviour
     [SerializeField]
     private AudioClip[] m_audioClips;
 
+    [SerializeField] 
+    private int m_audioSourceCount = 0;
+
+    private int m_currentIndex = 0;
+
     public enum SoundEffectName
     {
         cofirm,
@@ -21,18 +26,65 @@ public class SEManager : MonoBehaviour
         seaContact,
         CountDown
     }
+
+    private void Awake()
+    {
+        m_audioSources = new AudioSource[m_audioSourceCount];
+        for (int i = 0; i < m_audioSourceCount; i++)
+        {
+            var source = gameObject.AddComponent<AudioSource>();
+            source.playOnAwake = false;
+            m_audioSources[i] = source;
+        }
+    }
+
     public void OnPlayOneShot(SoundEffectName seNum)
     {
-        m_audioSources[(int)seNum].PlayOneShot(m_audioClips[(int)seNum]);
+        var source = GetFreeAudioSource();
+
+        source.PlayOneShot(m_audioClips[(int)seNum]);
+
+        m_currentIndex++;
+        if (m_currentIndex >= m_audioSources.Length)
+        {
+            m_currentIndex = 0;
+        }
     }
+
     public void OnPlay(SoundEffectName seNum)
     {
-        m_audioSources[(int)seNum].clip = m_audioClips[(int)seNum];
-        m_audioSources[(int)seNum].loop = true;
-        m_audioSources[(int)seNum].Play();
+        var source = GetFreeAudioSource();
+
+        source.clip = m_audioClips[(int)seNum];
+        source.loop = true;
+        source.Play();
+
+        m_currentIndex++;
+
+        if (m_currentIndex >= m_audioSources.Length)
+        {
+            m_currentIndex = 0;
+        }
     }
-    public void OnStop(SoundEffectName seNum)
+
+    private AudioSource GetFreeAudioSource()
     {
-        m_audioSources[(int)(seNum)].Stop();
+        int startIndex = m_currentIndex;
+
+        while (m_audioSources[m_currentIndex].isPlaying)
+        {
+            m_currentIndex = (m_currentIndex + 1) % m_audioSources.Length;
+            if (m_currentIndex == startIndex)
+                break;
+        }
+
+        AudioSource source = m_audioSources[m_currentIndex];
+        m_currentIndex = (m_currentIndex + 1) % m_audioSources.Length;
+        return source;
     }
 }
+
+//public void OnStop(SoundEffectName seNum)
+//{
+//    m_audioSources[(int)(seNum)].Stop();
+//}
