ad1044b372cd5775387e5980ba5cd0ad112bfd37
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-12-24 14:29:22 +0900

    [Fix]防御ボタンを離すことで攻撃するように修正しました。

--- Assets/MyProject/Player/Scripts/Test/OnClickAttack.cs
+++ Assets/MyProject/Player/Scripts/Test/OnClickAttack.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -19,48 +18,58 @@ public class OnClickAttack : MonoBehaviour
     {
         m_event = GetComponent<PlayerEvents>();
         m_blownAway = GetComponent<PlayerBlownAway>();
-        m_event.OnBlownAwayCanceled.AddListener(BlowCancelAttack);
+        m_event.OnDefenceEnd.AddListener(BlowCancelAttack);
+        //m_event.OnBlownAwayCanceled.AddListener(BlowCancelAttack);
         
     }
     private void Update()
     {
-        if (!isInited)
-        {
-            m_gamepad = GamepadManager.Instance.GetGamepad(0);
-            isInited = true;
-        }
+        //if (!isInited)
+        //{
+        //    m_gamepad = GamepadManager.Instance.GetGamepad(0);
+        //    isInited = true;
+        //}
 
-        if (Input.GetMouseButtonUp(0))
-        {
+        //if (Input.GetMouseButtonUp(0))
+        //{
 
-            if (m_blownAway.IsBlownAway)
-            {
-                m_event.OnBlownAwayCanceled?.Invoke();
-               // m_event.OnAttack?.Invoke();
-            }
+        //    if (m_blownAway.IsBlownAway)
+        //    {
+        //        m_event.OnBlownAwayCanceled?.Invoke();
+        //       // m_event.OnAttack?.Invoke();
+        //    }
 
-        }
+        //}
 
-        if(m_gamepad != null)
-        {
-            if (m_gamepad.bButton.wasPressedThisFrame)
-            {
+        //if(m_gamepad != null)
+        //{
+        //    if (m_gamepad.bButton.wasPressedThisFrame)
+        //    {
 
-                if (m_blownAway.IsBlownAway)
-                {
-                    m_event.OnBlownAwayCanceled?.Invoke();
-                    // m_event.OnAttack?.Invoke();
-                }
-            }
-        }
+        //        if (m_blownAway.IsBlownAway)
+        //        {
+        //            m_event.OnBlownAwayCanceled?.Invoke();
+        //            // m_event.OnAttack?.Invoke();
+        //        }
+        //    }
+        //}
 
-        //isBlownAwayかつクリックしたら攻撃isBlownAway解除
-        //攻撃演出
+        ////isBlownAwayかつクリックしたら攻撃isBlownAway解除
+        ////攻撃演出
+    }
+
+    private void DefenceEndAttack()
+    {
+
     }
 
     private void BlowCancelAttack()
     {
-        m_blownAway.StopBlownAway();
-        Instantiate(m_atkPrefab, transform.position, Quaternion.identity, this.transform);
+        if (m_blownAway.IsBlownAway)
+        {
+            m_blownAway.StopBlownAway();
+            Instantiate(m_atkPrefab, transform.position, Quaternion.identity, this.transform);
+        }
     }
 }
+//吹き飛び状態の時にDefenceEndが入力されたらOnBlownAwayCanceled?.Invoke()
