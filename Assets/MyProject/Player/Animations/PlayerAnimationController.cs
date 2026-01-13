ca07e622cd2ca015d883b239c2b9ccadf836766e
harunanagaoka <shinkaigyo428@i.softbank.jp>
2026-01-13 16:46:08 +0900

    [Add]PlayerAnimation追加

---
+++ Assets/MyProject/Player/Animations/PlayerAnimationController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private PlayerEvents m_playerEvents;
+    private Animator m_animator;
+    void Start()
+    {
+        m_playerEvents = GetComponentInParent<PlayerEvents>();
+        m_animator = GetComponent<Animator>();
+        m_playerEvents.OnStartMove.AddListener(SetWalk);
+        m_playerEvents.OnStop.AddListener(SetIdle);
+        m_playerEvents.OnBlownAwayCanceled.AddListener(SetAttack);
+        m_playerEvents.OnDamage.AddListener(SetDamage);
+    }
+
+    private void SetIdle()
+    {
+        m_animator.SetBool("Idle", true);
+        m_animator.SetBool("Move", false);
+        m_animator.SetBool("Damage", false);
+        m_animator.SetBool("Attack", false);
+    }
+    
+    private void SetAttack()
+    {
+        m_animator.SetBool("Idle", false);
+        m_animator.SetBool("Move", false);
+        m_animator.SetBool("Damage", false);
+        m_animator.SetBool("Attack", true);
+    }
+
+    private void SetDamage()
+    {
+        m_animator.SetBool("Idle", false);
+        m_animator.SetBool("Move", false);
+        m_animator.SetBool("Damage", true);
+        m_animator.SetBool("Attack", false);
+    }
+
+    private void SetWalk()
+    {
+        m_animator.SetBool("Idle", false);
+        m_animator.SetBool("Move", true);
+        m_animator.SetBool("Damage", false);
+        m_animator.SetBool("Attack", false);
+    }
+}
