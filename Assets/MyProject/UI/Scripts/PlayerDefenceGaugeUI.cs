83ca9eee6289934f8082e5e3ea69cddde1232232
karin_miyakawa <miyakawakarinn@gmail.com>
2026-01-22 14:10:14 +0900

    ガードゲージの連携

---
+++ Assets/MyProject/UI/Scripts/PlayerDefenceGaugeUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerDefenceGaugeUI : MonoBehaviour
+{
+    private PlayerDefenceGauge m_defenceGauge;
+
+    [SerializeField]
+    private UnityEngine.UI.Slider m_defenceGaugeSlider;
+
+    private void Start()
+    {
+        if (PlayerManager.Instance != null)
+        {
+            PlayerManager.Instance.OnResisterPlayer += Initialize;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (PlayerManager.Instance != null)
+        {
+            PlayerManager.Instance.OnResisterPlayer -= Initialize;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_defenceGaugeSlider.value = m_defenceGauge.DefencePercentage;
+    }
+
+    private void Initialize()
+    {
+        m_defenceGauge = PlayerManager.Instance.Players[0].GetComponent<PlayerDefenceGauge>();
+        m_defenceGaugeSlider.value = m_defenceGauge.DefencePercentage;
+    }
+}
