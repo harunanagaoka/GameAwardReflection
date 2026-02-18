efaea8046d7ae716e1e974c1622fc1af38d9e4f4
harunanagaoka <shinkaigyo428@i.softbank.jp>
2026-02-18 14:40:38 +0900

    [Fix]バグ解消のため一時的にスクリプトの一部をコメントアウトしました

--- Assets/MyProject/AudioSystem/Scripts/HurtSoundSwitcher.cs
+++ Assets/MyProject/AudioSystem/Scripts/HurtSoundSwitcher.cs
@@ -22,7 +22,7 @@ public class HurtSoundSwitcher : MonoBehaviour
         // 共通のダメージSEを鳴らす（例: PlayerNumb）
         if (m_seManager != null)
         {
-            m_seManager.OnPlayOneShot(SEManager.SoundEffectName.BossPunchHit);
+            //m_seManager.OnPlayOneShot(SEManager.SoundEffectName.BossPunchHit);
         }
     }
 }
