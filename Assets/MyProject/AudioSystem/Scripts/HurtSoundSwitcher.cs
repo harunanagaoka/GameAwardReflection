72797176dd23115e4d78b99a0a182a4ea36219bd
karin_miyakawa <miyakawakarinn@gmail.com>
2026-02-18 12:36:46 +0900

    攻撃を受けた際のSE再生、 プレイヤーのプレハブにスクリプトの追加

--- Assets/MyProject/AudioSystem/Scripts/HurtSoundSwitcher.cs
+++ Assets/MyProject/AudioSystem/Scripts/HurtSoundSwitcher.cs
@@ -9,55 +9,20 @@ public class HurtSoundSwitcher : MonoBehaviour
     private void Start()
     {
         m_seManager = Object.FindFirstObjectByType<SEManager>();
-        m_playerEvents.OnDamage.AddListener(OnDamageReceived);
-    }
-
-    public enum AttackType
-    { 
-        None,
-        RocketPunch,
-        ElectricGun,
-        ElectricFieldAttack
-    }
-
-    private AttackType ConvertAttackType(string tag)
-    {
-        switch (tag)
+        m_playerEvents = GetComponent<PlayerEvents>();
+        if (m_playerEvents != null)
         {
-            case "RocketPunch":
-                return AttackType.RocketPunch;
-            case "Undefined":
-                return AttackType.ElectricGun;
-            case "ElectricFieldAttack":
-                return AttackType.ElectricFieldAttack;
-            default:
-                return AttackType.None;
+            m_playerEvents.OnDamage.AddListener(OnDamageReceived);
         }
     }
 
-    private void PlayHurtSE(AttackType type)
+    // 引数なしに修正
+    public void OnDamageReceived()
     {
-        switch (type)
+        // 共通のダメージSEを鳴らす（例: PlayerNumb）
+        if (m_seManager != null)
         {
-            case AttackType.RocketPunch:
-                m_seManager.OnPlayOneShot(SEManager.SoundEffectName.BossPunchHit);
-                break;
-            case AttackType.ElectricGun:
-                m_seManager.OnPlayOneShot(SEManager.SoundEffectName.BossElectricGun);
-                break;
-            case AttackType.ElectricFieldAttack:
-                m_seManager.OnPlayOneShot(SEManager.SoundEffectName.PlayerNumb);
-                break;
+            m_seManager.OnPlayOneShot(SEManager.SoundEffectName.BossPunchHit);
         }
     }
-
-    private void OnDamageReceived(Collider other)
-    {
-        AttackType attackType = ConvertAttackType(other.tag);
-        if (attackType != AttackType.None)
-        {
-            PlayHurtSE(attackType);
-        }
-    }
-
 }
