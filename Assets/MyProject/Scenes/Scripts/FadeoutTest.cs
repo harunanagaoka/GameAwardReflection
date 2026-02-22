9fd55822755a5a6c71534c24af4b1b8575df19c2
harunanagaoka <shinkaigyo428@i.softbank.jp>
2026-02-22 14:00:50 +0900

    不要なusingの削除

--- Assets/MyProject/Scenes/Scripts/FadeoutTest.cs
+++ Assets/MyProject/Scenes/Scripts/FadeoutTest.cs
@@ -1,7 +1,5 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
