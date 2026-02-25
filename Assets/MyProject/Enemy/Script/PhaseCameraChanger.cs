7a3ee2c82d8a32f6410e79328a923cd6b75af372
karin_miyakawa <miyakawakarinn@gmail.com>
2026-02-25 16:53:45 +0900

    フェーズ変更時のカメラ切り替え処理のスクリプトを作成

---
+++ Assets/MyProject/Enemy/Script/PhaseCameraChanger.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    public Camera cameraA;
+    [SerializeField]
+    public Camera cameraB;
+    [SerializeField]
+    public GameObject canvas;
+    [SerializeField]
+    float opentime = 5f;
+
+    private int cameraIndex = 1;
+    bool iscorutineRunning = false;
+
+    void Start()
+    {
+        canvas.SetActive(false);
+        cameraA.enabled = true;
+        cameraB.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !iscorutineRunning)
+        {
+            StartCoroutine(PhaseChanger());
+            iscorutineRunning = true;
+        }
+    }
+
+    IEnumerator PhaseChanger()
+    {
+        if (cameraIndex == 1)
+        {
+            // カメラBを有効にし、Canvasを表示
+            cameraA.enabled = false;
+            cameraB.enabled = true;
+            canvas.SetActive(true);
+            cameraIndex = 2;
+
+            yield return new WaitForSeconds(opentime);
+
+            // カメラAに戻し、Canvasを非表示
+            cameraA.enabled = true;
+            cameraB.enabled = false;
+            canvas.SetActive(false);
+            cameraIndex = 1;
+        }
+        iscorutineRunning = false;
+        yield return null;
+    }
+}
